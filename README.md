0a27c453ad04e2777f0ad2671722dafb81e214e2
Alireza Nourian <az.nourian@gmail.com>
2015-08-01 07:29:44 +0430

    add main template

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 
 # Moratab server
+
+    heroku config:set PATH=/app/.apt/usr/local/bin:/app/.heroku/python/bin:/app/.apt/usr/bin:/usr/local/bin:/usr/bin:/bin
--- main.py
+++ main.py
@@ -1,14 +1,38 @@
 
+import moratab
 import pdfkit
-from flask import Flask, make_response
+from flask import Flask, request, render_template, make_response
 app = Flask(__name__)
 app.config['PROPAGATE_EXCEPTIONS'] = True
 
 
+css_file = 'static/main.css'
+pdf_file = 'static/document.pdf'
+options = {
+	'margin-top': '0.75in',
+	'margin-right': '0.75in',
+	'margin-left': '0.75in',
+	'margin-bottom': '0.75in',
+}
+
+
 @app.route('/')
-def main(url=None):
-	pdf_file = 'static/document.pdf'
-	pdfkit.from_url('http://google.com', pdf_file)
+def main():
+	return 'Moratab Server!'
+
+
+@app.route('/html', methods=['POST'])
+def html():
+	content = moratab.render(request.form['moratab'])
+	content = '<link type="text/css" rel="stylesheet" href="{0}">'.format(css_file) + content
+	return render_template('main.html', content=content)
+
+
+@app.route('/pdf', methods=['POST'])
+def pdf():
+	content = moratab.render(request.form['moratab'])
+	html = render_template('main.html', content=content)
+	pdfkit.from_string(html, pdf_file, options=options, css=css_file)
 	response = make_response(open(pdf_file).read())
 	response.content_type = 'application/pdf'
 	return response
